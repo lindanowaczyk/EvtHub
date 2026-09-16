d61fda3c3f1475adc0779fa36c01be745838fecf
lindanowaczyk <vitorianowaczyk@gmail.com>
2026-09-16 01:28:55 -0300

    fix: update secret option

--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   imports: [
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secret',
+      secret: process.env.JWT_SECRET || 'secret-key',
       signOptions: { expiresIn: '1d' },
     }),
   ],
